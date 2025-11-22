b016efb25bfadfa2fe33425cd4cb272d130c2763
Ben Huvelle <benoit.huvelle@gmail.com>
2025-11-22 15:32:53 +0100

    change colors to object

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Badge } from "./components/ui/badge.tsx";
+import { Label } from "./components/ui/label.tsx";
 import { DEFAULT_FILL_COLOR, GRID_SIZE } from "./constants.ts";
 import "./App.css";
 import colors from "./colors.ts";
@@ -43,13 +44,17 @@ function App() {
 
         <Badge>Pick a color</Badge>
         <div className="grid grid-cols-5">
-          {Object.keys(colors).map((color) => (
-            <div
-              key={color}
-              className="w-10 h-10 m-4 rounded-full border-4 shadow border-white hover:scale-110 duration-75 cursor-pointer"
-              style={{ backgroundColor: colors[color] }}
-              onClick={() => setFillColor(colors[color])}
-            />
+          {Object.values(colors).map((color) => (
+            <div key={color.code} className="flex flex-col items-center m-4">
+              <div
+                className="w-10 h-10 rounded-full border-4 shadow border-white hover:scale-110 duration-75 cursor-pointer"
+                style={{ backgroundColor: color.hexa }}
+                onClick={() => setFillColor(color.hexa)}
+              />
+              <Label className="text-xs mt-2 text-center">
+                {color.name} {color.code}
+              </Label>
+            </div>
           ))}
         </div>
       </div>
--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,6 @@ import colors from "./colors";
 
 export const PIXEL_SIZE = 11.55;
 export const GRID_SIZE = 10;
-export const DEFAULT_FILL_COLOR = colors.WHITE;
-export const DEFAULT_STROKE_COLOR = colors.BLACK;
+export const DEFAULT_FILL_COLOR = colors.WHITE.hexa;
+export const DEFAULT_STROKE_COLOR = colors.BLACK.hexa;
 export const STROKE_WIDTH = 0.03;
